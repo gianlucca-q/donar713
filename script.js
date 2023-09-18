b4105dfbcc37059af9d3324aaac15b76ba567da4
Gianlucca Quinteros <70190867+gianlucca-q@users.noreply.github.com>
2023-09-18 18:18:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function onMenuClick() {
+    var navbar = document.getElementById("navigation-bar");
+    var navbar2 = document.getElementById("navigation-bar2");
+    var responsive_class_name = "responsive";
+
+    navbar.classList.toggle(responsive_class_name);
+    navbar2.classList.toggle(responsive_class_name);
+}
